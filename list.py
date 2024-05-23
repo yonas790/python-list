62f6ff2ca3abc2508fea0902556fbb2313393b0b
yonas790 <70878858+yonas790@users.noreply.github.com>
2024-05-23 19:47:27 +0300

    uploading

---
+++ list.py
@@ -0,0 +1,17 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.extend([50, 60, 70])
+print(my_list)
+my_list.insert(1, 15) #inserting the value 15 on the second position of the list
+print(my_list)
+my_list.extend([50,60,70])
+print(my_list)
+my_list.remove(70)  #removing the last element form the list
+my_list.sort()
+print(my_list)
+print(my_list[3]) #displays 30
+
